5141dfa27ed0b4628c70f363cb5cd6e3de63ee8b
Bernard Anderson <bernard.anderson@gmail.com>
2016-02-28 22:27:16 -0600

    added listeners for output to mimic textbox

--- events.js
+++ events.js
@@ -1,6 +1,8 @@
 var articleSectionArray = document.getElementsByClassName("article-section");
 var outputTarget = document.getElementById("output-target");
 var h1Title = document.getElementById("page-title");
+var textBoxInput = document.getElementById("keypress-input");
+
 
 // When this function gets called in the for loop below, 'i' gets passed to it during the original 
 //  call.  It returns an anon func the has the [i] element evaluated.
@@ -16,9 +18,16 @@ function outputChange(textString) {
   };
 }
 
+function outputMimicTextBox() {
+  return function() {
+    outputTarget.textContent = textBoxInput.value;
+  };
+}
+
 for (var i=0; i < articleSectionArray.length; i++) {
        articleSectionArray[i].addEventListener("click", reportSectionClick(i));
 };
 
 h1Title.addEventListener("mouseover", outputChange("You moved your mouse over the header"));
 h1Title.addEventListener("mouseout", outputChange("You left me!!"));
+textBoxInput.addEventListener("input", outputMimicTextBox());
