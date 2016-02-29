4c25392a57ad1ab05b2cbd9eb53ba9579a39ba5c
Bernard Anderson <bernard.anderson@gmail.com>
2016-02-28 22:15:49 -0600

    added listeners for h1 mouseover and out

--- events.js
+++ events.js
@@ -1,15 +1,24 @@
 var articleSectionArray = document.getElementsByClassName("article-section");
 var outputTarget = document.getElementById("output-target");
+var h1Title = document.getElementById("page-title");
 
 // When this function gets called in the for loop below, 'i' gets passed to it during the original 
 //  call.  It returns an anon func the has the [i] element evaluated.
-function reportClick(i) {
+function reportSectionClick(i) {
   return function(){
     outputTarget.textContent = "you clicked on the '" + articleSectionArray[i].textContent + "' section";
   };
 }
 
+function outputChange(textString) {
+  return function() {
+    outputTarget.textContent = textString;
+  };
+}
+
 for (var i=0; i < articleSectionArray.length; i++) {
-       articleSectionArray[i].addEventListener("click", reportClick(i));
+       articleSectionArray[i].addEventListener("click", reportSectionClick(i));
 };
 
+h1Title.addEventListener("mouseover", outputChange("You moved your mouse over the header"));
+h1Title.addEventListener("mouseout", outputChange("You left me!!"));
